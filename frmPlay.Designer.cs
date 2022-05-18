a5217595aac84cec510a4c0c3e0ceb98d3b0d3ad
ngochai-hcmus <104615099+ngochai-hcmus@users.noreply.github.com>
2022-05-19 00:33:39 +0700

    Add files via upload

---
+++ frmPlay.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace JigsawPuzzle
+{
+    partial class frmPlay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.pnlPuzzel = new System.Windows.Forms.Panel();
+            this.pnlPlay = new System.Windows.Forms.Panel();
+            this.pnlPicture = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pnlMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Controls.Add(this.pnlPuzzel);
+            this.pnlMain.Controls.Add(this.pnlPlay);
+            this.pnlMain.Controls.Add(this.pnlPicture);
+            this.pnlMain.Controls.Add(this.btnExit);
+            this.pnlMain.Controls.Add(this.btnStart);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(925, 708);
+            this.pnlMain.TabIndex = 0;
+            // 
+            // pnlPuzzel
+            // 
+            this.pnlPuzzel.BackColor = System.Drawing.SystemColors.Highlight;
+            this.pnlPuzzel.Location = new System.Drawing.Point(493, 12);
+            this.pnlPuzzel.Name = "pnlPuzzel";
+            this.pnlPuzzel.Size = new System.Drawing.Size(420, 420);
+            this.pnlPuzzel.TabIndex = 7;
+            // 
+            // pnlPlay
+            // 
+            this.pnlPlay.BackColor = System.Drawing.Color.Bisque;
+            this.pnlPlay.Location = new System.Drawing.Point(12, 12);
+            this.pnlPlay.Name = "pnlPlay";
+            this.pnlPlay.Size = new System.Drawing.Size(420, 420);
+            this.pnlPlay.TabIndex = 6;
+            // 
+            // pnlPicture
+            // 
+            this.pnlPicture.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlPicture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlPicture.Location = new System.Drawing.Point(12, 446);
+            this.pnlPicture.Name = "pnlPicture";
+            this.pnlPicture.Size = new System.Drawing.Size(250, 250);
+            this.pnlPicture.TabIndex = 5;
+            this.pnlPicture.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pnlPicture_MouseClick);
+            this.pnlPicture.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlPicture_MouseMove);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(741, 620);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(118, 56);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(741, 541);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(118, 59);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start Game";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // frmPlay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 708);
+            this.Controls.Add(this.pnlMain);
+            this.Name = "frmPlay";
+            this.Text = "frmPlay";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmPlay_FormClosing);
+            this.pnlMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Panel pnlPuzzel;
+        private System.Windows.Forms.Panel pnlPlay;
+        private System.Windows.Forms.Panel pnlPicture;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ frmPlay.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JigsawPuzzle
+{
+    public partial class frmPlay : Form
+    {
+        public frmPlay()
+        {
+            InitializeComponent();
+
+        }
+
+        #region Properties
+        private Bitmap ImageFile;
+        private string fileName = Application.StartupPath + "\\Resources\\Picture.bmp";
+        private List<PictureBox> picture = new List<PictureBox>();
+        private int cellSize;
+        private Point picLocation;
+
+        #endregion
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmPlay_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult d = MessageBox.Show("\t Bạn có muốn thoát? \t", "Thoát", MessageBoxButtons.YesNo);
+            if (d == DialogResult.No)
+                e.Cancel = true;
+        }
+
+        private void pnlPicture_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                int x = e.Y + pnlPicture.Top - pnlPicture.Height / 2;
+                int y = e.X + pnlPicture.Left - pnlPicture.Width / 2;
+
+                if (y < 12) y = 12;
+                if (y > 485) y = 485;
+                if (x != 446) x = 446;
+
+                pnlPicture.BringToFront();
+
+                pnlPicture.Top = x;
+                pnlPicture.Left = y;
+            }
+        }
+
+        private void pnlPicture_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                OpenFileDialog dlgOpen = new OpenFileDialog();
+                dlgOpen.RestoreDirectory = true;
+                dlgOpen.Filter = "JPEG Files(*.png)|*.png|JPEG Files(*.jpg)|*.jpg|GIF Files(*.gif)|*.gif|All Files|*.*";
+                dlgOpen.FilterIndex = 1;
+                if (dlgOpen.ShowDialog() == DialogResult.OK)
+                {
+                    fileName = dlgOpen.FileName;
+                    //cắt ảnh cho vừa đủ để xử lý
+                    cutPicture();
+                    
+                }
+            }
+        }
+
+        private void cutPicture ()
+        {
+            if (!System.IO.File.Exists(fileName))
+            {
+                MessageBox.Show("Không tìm thấy file \n" + fileName);
+                return;
+            }
+            try
+            {
+                //cắt ảnh thành hình vuông
+                Bitmap bmp = new Bitmap(fileName);
+
+                float minSize;
+                float width, height;
+
+                if (bmp.Size.Width < bmp.Size.Height)
+                    minSize = bmp.Size.Width;
+                else
+                    minSize = bmp.Size.Height;
+
+                width = bmp.Size.Width;
+                height = bmp.Size.Height;
+
+                float heso = 350 / minSize;
+                if (heso > 0)
+                {
+                    width *= heso;
+                    height *= heso;
+                }
+
+                int imageSize;
+                ImageFile = new Bitmap(bmp, new Size((int)width, (int)height));
+                
+                if (ImageFile.Width < ImageFile.Height)
+                    imageSize = ImageFile.Width;
+                else
+                    imageSize = ImageFile.Height;
+                imageSize -= imageSize % 4;
+                cellSize = imageSize / 4;
+
+                //fix ảnh cho thành hình vuông
+                //ImageFile = new Bitmap(bmp, new Size((int)imageSize, (int)imageSize));
+                
+                //cắt ảnh thành hình vuông
+                
+                Rectangle imageRect = new Rectangle(0, 0, imageSize, imageSize);
+                ImageFile = ImageFile.Clone(imageRect, PixelFormat.DontCare);
+
+                pnlPicture.BackgroundImage = ImageFile;
+
+                //cắt ảnh thành từng mảnh
+                int cellIndex = 0;
+                int boxSize = 420 / 4;
+
+                //xóa picturebox đã lưu trước vào panel
+                if(picture.Count != 0)
+                {
+                    for (int i = 0; i < 4 * 4; i++)
+                        pnlMain.Controls.Remove(picture[i]);
+                    picture.RemoveRange(0, 4 * 4);
+                    pnlPlay.Controls.Clear();
+                    pnlPlay.Invalidate();
+                }
+                picture.Clear();
+
+                //lưu các picturebox vào panel
+                for (int j = 0; j < 4; j++)
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        try
+                        {   
+                            picture.Add(new PictureBox());
+                            imageRect = new Rectangle(i * cellSize, j * cellSize, cellSize, cellSize);
+                            picture[cellIndex].BackgroundImage = null;
+                            picture[cellIndex].BackgroundImage = ImageFile.Clone(imageRect, PixelFormat.DontCare);
+                            picture[cellIndex].Location = new Point(493 + i * (boxSize), 12 + j * (boxSize));
+                            picture[cellIndex].Size = new Size(boxSize, boxSize);
+                            picture[cellIndex].Tag = cellIndex.ToString();
+                            picture[cellIndex].BackgroundImageLayout = ImageLayout.Stretch;
+                            pnlMain.Controls.Add(picture[cellIndex]);
+                            picture[cellIndex].BringToFront();
+                            picture[cellIndex].MouseMove += FrmPlay_MouseMove;
+                            picture[cellIndex].MouseUp += FrmPlay_MouseUp;
+                            picture[cellIndex].MouseDown += FrmPlay_MouseDown;
+                            cellIndex++;
+                        }
+                        catch (Exception e)
+                        {
+                            MessageBox.Show(e.Message);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void FrmPlay_MouseDown(object sender, MouseEventArgs e)
+        {
+            //lưa vị trí của pic
+            PictureBox pic = (PictureBox)sender;
+            picLocation = pic.Location;
+            pic.BringToFront();
+        }
+
+        private void FrmPlay_MouseUp(object sender, MouseEventArgs e)
+        {
+            PictureBox pic = (PictureBox)sender;
+            int BoxSize = 420 / 4;
+            int col = pic.Location.X / BoxSize;
+            int row = pic.Location.Y / BoxSize;
+
+            if (col >= 4 || row >= 4)
+                return;
+
+            Control ctl = pnlMain.GetChildAtPoint(new Point(col * BoxSize + 12, row * BoxSize + 12));
+            // Nếu đã có một khung ảnh tại ô này
+            // thì chuyển vị trí của khung ảnh này về vị trí của khung ảnh vừa drop
+            if (ctl != null && ctl is PictureBox)
+            {
+                ctl.Location = picLocation;
+            }
+
+            pic.Location = new Point(col * BoxSize + 12, row * BoxSize + 12);
+        }
+
+        private void FrmPlay_MouseMove(object sender, MouseEventArgs e)
+        {
+            PictureBox pic = sender as PictureBox;
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                pic.BringToFront();
+                int x = e.Y + pic.Top - pic.Height / 2;
+                int y = e.X + pic.Left - pic.Width / 2;
+                pic.Top = x;
+                pic.Left = y;
+            }
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            cutPicture(); 
+        }
+    }
+}
